1d709b99f47f4a142632db5f36685cbd00cfcea4
senwu <senwu@stanford.edu>
2016-09-23 23:53:08 -0700

    add KgramMatcher

--- snorkel/matchers.py
+++ snorkel/matchers.py
@@ -245,6 +245,18 @@ class NumberMatcher(Matcher):
         except:
             return False
 
+class KgramMatcher(Matcher):
+    """Matches all K or less grams"""
+    def init(self):
+        try:
+            self.k = self.opts['k']
+        except KeyError:
+            raise Exception("Please supply a gram length k.")
+    def _f(self, c):
+        if len(c.idxs) <= self.k:
+            return True
+        else:
+            return False
 
 class CandidateExtractor(object):
     """Temporary class for interfacing with the post-candidate-extraction code"""
