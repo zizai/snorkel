2988560cd2c99da166c47022d8108025befeb2db
Xiao Cheng <xiao2@stanford.edu>
2017-01-03 18:47:16 -0800

    Updated synthetic test to include all 6 figures

--- test/SchemaTradeoffTest.py
+++ test/SchemaTradeoffTest.py
@@ -1,21 +1,15 @@
 import numpy as np
 import timeit
 import matplotlib.pyplot as plt
-import warnings
+from collections import defaultdict
 
-warnings.filterwarnings('ignore', 
-                        '.*', 
-                        UserWarning,
-                        'warnings_filtering',
-                        )
 
 candidate_count = 10
-feat_count = 10000
 
 iterations = [1, 3, 10, 30, 100]
-inv_densities = [1, 10, 100, 1000]
-scale = [100, 1000, 10000]
-default_inv_density = 100
+inv_densities = [100, 10, 1]
+feat_sizes = [100, 1000, 10000]
+default_inv_density = inv_densities[0]
 
 def new_immutable_vec(size):
     return tuple(range(size))
@@ -66,70 +60,73 @@ class dense():
     def query(self):
         return self.data[0]
 
-def show_plot(xname):
+def show_plot(xname, yname):
     axes = plt.gca()
     caption = '%dx%d, density=%s' % (candidate_count, feat_count, str(1.0/default_inv_density))
     axes.set_title(caption)
-    axes.set_xlabel(xname.title() +'(log scale)')
-    axes.set_ylabel('Execution time(log scale)')
+    axes.set_xlabel(xname.title())
+    axes.set_ylabel(yname.title())
     plt.xscale('log', nonposy='clip')
     plt.yscale('log', nonposy='clip')
     plt.legend(loc='upper left')
     plt.show()
     
-def run_benchmark(benchmark):    
-    series = {}
+def run_benchmark(xaxis, yaxis):    
+    series = defaultdict(list)
     mat_types = [dense, coo, lil]
     
-    if benchmark == 'init':
-        xs = scale
-    elif benchmark == 'iterations':
+    if xaxis == 'feat_size':
+        xs = feat_sizes
+    elif xaxis == 'iterations':
         xs = iterations
-    elif benchmark == 'sparsity':
+    elif xaxis == 'sparsity':
         xs = inv_densities
         
     for mat in mat_types:
         mat_name = mat.__name__
-        series[mat_name] = []
         # x is the variable on x-axis for plotting
         for x in xs:
             repetition = 10
             exeuctions = 1
-            if benchmark == 'init':
-                run = lambda: mat(x, feat_count)
-            elif benchmark == 'iterations':
+            if xaxis == 'feat_size':
+                if yaxis == 'mat_time':
+                    run = lambda: mat(candidate_count, x)
+                else:
+                    m = mat(candidate_count, feat_sizes[0])
+                    run = lambda: m.query() 
+            elif xaxis == 'iterations':
                 def run():
                     # Here we simulate higher dimension with dense matrix
-                    m = mat(candidate_count, feat_count)
+                    m = mat(candidate_count, feat_sizes[0])
                     for _ in xrange(x):
                         m.add_col(1)
-            elif benchmark == 'sparsity':
+            elif xaxis == 'sparsity':
                 repetition = 100
                 exeuctions = 100
-                m = mat(candidate_count, feat_count, x)
+                m = mat(candidate_count, feat_sizes[0], x)
                 run = lambda: m.query()
+                
+               
             timings = timeit.Timer(run).repeat(repetition, exeuctions)
             # min timings are indicative of the hardware limitations
             series[mat_name].append(min(timings))
         plt.plot(xs, series[mat_name], label=mat_name)
+    
+    # Ad-hoc postprocessings    
     # For plotting purposes we use actual sparsity
-    if xs is inv_densities:
-        xs = [1.0/x for x in xs]
-    print 'Benchmark_' + benchmark + '\t' + '\t'.join([str(x) for x in xs])
+    if xs is inv_densities: xs = [1.0/x for x in xs]
+        
+    print 'Benchmark_' + xaxis + '\t' + '\t'.join([str(x) for x in xs])
     for mat_name, data in series.iteritems():
         print mat_name + '\t' + '\t'.join([str(x) for x in data])
-    show_plot(benchmark)
-#     plt.clf()
-#                 print '\t'.join([mat, '%dx%d' % (size, size), str(iteration), str(avg) + 's'])
-#                 print 'mat type', mat
-#                 print 'size %dx%d' % (size, size)
-#                 print 'col add iterations', iteration
-#                 print 'avg time', avg
+    show_plot(xaxis, yaxis)
 
-#     plt.show()
 
 if __name__ == '__main__':
-    run_benchmark('iterations')
-    run_benchmark('sparsity')
+    xaxis = ['feat_size', 'iterations', 'sparsity']
+    yaxis = ['query_time','mat_time']
+    for y in yaxis:
+        for x in xaxis:
+            run_benchmark(x, y)
     
     
