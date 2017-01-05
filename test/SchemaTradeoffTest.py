8a7744b652498b5a8a7a7ac797e22ae85477ca3c
Xiao Cheng <xiao@cs.stanford.edu>
2017-01-04 21:11:08 -0800

    Updated design consideration exp code

--- test/SchemaTradeoffTest.py
+++ test/SchemaTradeoffTest.py
@@ -6,18 +6,21 @@ from collections import defaultdict
 
 candidate_count = 10
 
+# Parameter list
+feat_sizes = [1000, 3000, 10000, 30000, 100000]
 iterations = [1, 3, 10, 30, 100]
-inv_densities = [100, 10, 1]
-feat_sizes = [100, 1000, 10000]
-default_inv_density = inv_densities[0]
+inv_densities = [1000, 100, 30, 10, 1]
+
+default_inv_density = 100
+default_feat_size = 100
 
 def new_immutable_vec(size):
     return tuple(range(size))
 
 class lil():
     
-    def __init__(self, n, m, inv_sparsity = default_inv_density):
-        m/=inv_sparsity
+    def __init__(self, n, m, sparsity = default_inv_density):
+        m/=sparsity
         self.data = [new_immutable_vec(m) for _ in xrange(n)]
         
     def add_col(self, size):
@@ -30,9 +33,9 @@ class lil():
         
 class coo():
     
-    def __init__(self, n, m, inv_sparsity = default_inv_density):
+    def __init__(self, n, m, sparsity = default_inv_density):
+        m/=sparsity
         # Simulate (id, key, val) schema
-        m/=inv_sparsity
         self.data = [(i, j) for i in xrange(n) for j in xrange(m)]
         self.row_count = n
         self.col_count = m
@@ -44,11 +47,11 @@ class coo():
                 self.data.append((r, new_key))
                 
     def query(self):
-        return list(self.data[:self.col_count])
+        return tuple(self.data[:self.col_count])
                 
 class dense():
     
-    def __init__(self, n, m, inv_sparsity = 1):
+    def __init__(self, n, m, sparsity = default_inv_density):
         # Simulate (id, key, val) schema
         self.data = np.random.rand(n, m)
         self.n = n
@@ -62,7 +65,7 @@ class dense():
 
 def show_plot(xname, yname):
     axes = plt.gca()
-    caption = '%dx%d, density=%s' % (candidate_count, feat_count, str(1.0/default_inv_density))
+    caption = ''#%dx%d, density=%s' % (candidate_count, feat_count, str(1.0/default_inv_density))
     axes.set_title(caption)
     axes.set_xlabel(xname.title())
     axes.set_ylabel(yname.title())
@@ -74,52 +77,59 @@ def show_plot(xname, yname):
 def run_benchmark(xaxis, yaxis):    
     series = defaultdict(list)
     mat_types = [dense, coo, lil]
-    
+    real_xs = None
     if xaxis == 'feat_size':
         xs = feat_sizes
     elif xaxis == 'iterations':
         xs = iterations
     elif xaxis == 'sparsity':
         xs = inv_densities
-        
+        real_xs = [1.0/x for x in xs]
+    if not real_xs: real_xs = xs
+
+    repetition = 10
+    exeuctions = 1        
     for mat in mat_types:
         mat_name = mat.__name__
         # x is the variable on x-axis for plotting
         for x in xs:
-            repetition = 10
-            exeuctions = 1
+            m = None
             if xaxis == 'feat_size':
                 if yaxis == 'mat_time':
                     run = lambda: mat(candidate_count, x)
                 else:
-                    m = mat(candidate_count, feat_sizes[0])
-                    run = lambda: m.query() 
+                    m = mat(candidate_count, x)
             elif xaxis == 'iterations':
-                def run():
-                    # Here we simulate higher dimension with dense matrix
-                    m = mat(candidate_count, feat_sizes[0])
-                    for _ in xrange(x):
-                        m.add_col(1)
+                if yaxis == 'mat_time':
+                    def run():
+                        # Here we simulate higher dimension with dense matrix
+                        m = mat(candidate_count, 1000, 10)
+                        for _ in xrange(x):
+                            m.add_col(1)
+                else:
+                    # Here we directly instantiate the larger matrix for query
+                    m = mat(candidate_count, default_feat_size + x, 1)
             elif xaxis == 'sparsity':
-                repetition = 100
-                exeuctions = 100
-                m = mat(candidate_count, feat_sizes[0], x)
-                run = lambda: m.query()
+                feat_size = max(inv_densities)
+                if yaxis == 'mat_time':
+                    run = lambda: mat(candidate_count, feat_size, x)
+                else:
+                    m = mat(candidate_count, feat_size, x)
                 
-               
+            if yaxis == 'query_time':
+                repetition = 1000
+                exeuctions = 100
+                run = lambda : m.query()
+                
             timings = timeit.Timer(run).repeat(repetition, exeuctions)
             # min timings are indicative of the hardware limitations
             series[mat_name].append(min(timings))
-        plt.plot(xs, series[mat_name], label=mat_name)
+        plt.plot(real_xs, series[mat_name], label=mat_name)
     
-    # Ad-hoc postprocessings    
-    # For plotting purposes we use actual sparsity
-    if xs is inv_densities: xs = [1.0/x for x in xs]
-        
-    print 'Benchmark_' + xaxis + '\t' + '\t'.join([str(x) for x in xs])
+    print 'Benchmark_' + xaxis + '\t' + '\t'.join([str(x) for x in real_xs])
     for mat_name, data in series.iteritems():
         print mat_name + '\t' + '\t'.join([str(x) for x in data])
-    show_plot(xaxis, yaxis)
+#     show_plot(xaxis, yaxis)
 
 
 if __name__ == '__main__':
@@ -127,6 +137,7 @@ if __name__ == '__main__':
     yaxis = ['query_time','mat_time']
     for y in yaxis:
         for x in xaxis:
-            run_benchmark(x, y)
+#             if x == 'iterations' and y == 'mat_time': 
+            run_benchmark(x, y) 
     
     
