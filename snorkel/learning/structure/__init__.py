75739ee5903726fb23aae36bfebec5bf68b95d55
Bryan He <bryanhe@stanford.edu>
2017-05-07 01:47:34 -0700

    copy dep selector

--- snorkel/learning/structure/__init__.py
+++ snorkel/learning/structure/__init__.py
@@ -2,5 +2,6 @@
 Subpackage for learning the structures of models.
 """
 from .gen_learning import DependencySelector
+from .coral_learning import CoralDependencySelector
 from .synthetic import generate_model, generate_label_matrix
 from .utils import *
