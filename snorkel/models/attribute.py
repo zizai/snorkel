65a7a5a9439e631360d014cc2f3a6316b2ea04b4
Jason Alan Fries <fries.jason@gmail.com>
2017-08-25 17:01:48 -0700

    Updated CDR for new SequenceTag candidate generation Interface with BioCorpus for scale-up experiements

---
+++ snorkel/models/attribute.py
@@ -0,0 +1,34 @@
+from sqlalchemy import (
+    Column, String, Integer, Float, Boolean, ForeignKey, UniqueConstraint,
+    MetaData
+)
+
+from .meta import SnorkelBase
+
+class ContextAttribute(SnorkelBase):
+    """
+    This table stores sequence tags from provided by NER taggers.
+    Tags are defined by document-level absolute character offsets.
+    """
+    __tablename__ = 'sequence_tag'
+    id = Column(Integer, primary_key=True)
+    document_id    = Column(Integer, ForeignKey('document.id'))
+    #document_id = Column(Integer, ForeignKey('document.id', ondelete='CASCADE'))
+    abs_char_start = Column(Integer, nullable=False)
+    abs_char_end   = Column(Integer, nullable=False)
+    concept_type   = Column(String, nullable=False) # , index=True
+    concept_uid    = Column(String, nullable=True)
+    source         = Column(String, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint(document_id, abs_char_start, abs_char_end, concept_type, source),
+    )
+
+    def __repr__(self):
+        return "<SequenceTag: {}({}:{}-{} [{}])>".format(self.concept_type, self.document_id,
+                                                         self.abs_char_start, self.abs_char_end,
+                                                         self.concept_uid if self.concept_uid else "NONE")
+
+
+
+
