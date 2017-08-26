d371a5bc7862bb9ced7bf5a0bfc6ed748153b8d3
Jason Alan Fries <fries.jason@gmail.com>
2017-08-25 18:03:31 -0700

    fixed UniqueConstraint on SequenceTag table

--- snorkel/models/tag.py
+++ snorkel/models/tag.py
@@ -13,7 +13,6 @@ class SequenceTag(SnorkelBase):
     __tablename__ = 'sequence_tag'
     id = Column(Integer, primary_key=True)
     document_id    = Column(Integer, ForeignKey('document.id'))
-    #document_id = Column(Integer, ForeignKey('document.id', ondelete='CASCADE'))
     abs_char_start = Column(Integer, nullable=False)
     abs_char_end   = Column(Integer, nullable=False)
     concept_type   = Column(String, nullable=False) # , index=True
@@ -21,10 +20,14 @@ class SequenceTag(SnorkelBase):
     source         = Column(String, nullable=False)
 
     __table_args__ = (
-        UniqueConstraint(document_id, abs_char_start, abs_char_end, concept_type, source),
+        UniqueConstraint(document_id, abs_char_start, abs_char_end, concept_type, concept_uid, source),
     )
 
     def __repr__(self):
         return "<SequenceTag: {}({}:{}-{} [{}])>".format(self.concept_type, self.document_id,
                                                          self.abs_char_start, self.abs_char_end,
                                                          self.concept_uid if self.concept_uid else "NONE")
+
+
+
+
