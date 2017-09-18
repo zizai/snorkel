285eff1e093a5e9b5e7d3d17a7f0815b99eeb204
Jason Alan Fries <fries.jason@gmail.com>
2017-09-18 10:43:31 -0700

    fixed fasttext embs in PCA

--- snorkel/contrib/pca/pca.py
+++ snorkel/contrib/pca/pca.py
@@ -162,8 +162,13 @@ class PCA(TFNoiseAwareModel):
 
         # Word embeddings
         f = open(self.word_emb_path, 'r')
+        fmt = "fastText" if self.word_emb_path.split(".")[-1] == "vec" else "txt"
 
-        for line in f:
+        for i,line in enumerate(f):
+
+            if fmt == "fastText" and i == 0:
+                continue
+
             line = line.strip().split(' ')
             if len(line) > self.word_emb_dim + 1:
                 line[0] = ' '
