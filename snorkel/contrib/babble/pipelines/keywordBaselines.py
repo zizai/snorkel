d80a81fec827514e9e7b9ee16490a6a681a66a24
emilydiana <emily.r.diana@gmail.com>
2017-11-06 11:39:57 -0800

    Empty file to be fleshed out

---
+++ snorkel/contrib/babble/pipelines/keywordBaselines.py
@@ -0,0 +1,4 @@
+### This file will contain a class definition to remove extra conditions
+### from explanantions containing a string literal. These modified explanations
+### will ultimately be used to construct baselines investigating whether
+### the extra condition information improves performance.
