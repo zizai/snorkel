c6a97a478a58d464a3a20f5359c1541501dc2511
emilydiana <emily.r.diana@gmail.com>
2017-11-06 21:43:05 -0800

    Able to find one or more keywords and then modify explanation to include just keyword in condition

--- snorkel/contrib/babble/pipelines/keywordBaselines.py
+++ snorkel/contrib/babble/pipelines/keywordBaselines.py
@@ -1,5 +1,5 @@
 from snorkel.contrib.babble import Explanation, link_explanation_candidates
-
+import re
 ###from snorkel.contrib.babble.semparser import Explanation
 ### This file will contain a class definition to remove extra conditions
 ### from explanantions containing a string literal. These modified explanations
@@ -17,17 +17,23 @@ class KeywordBaselines:
 	self.label=explanation.label
 	self.semantics=explanation.semantics
 
+    def findKeywords(self):
+	keyword=re.findall(r'\'(.+?)\'',self.condition)
+	return keyword
+
     def modify(self, keyword):
 	newExp = Explanation(name = self.name, condition = keyword, candidate = self.candidate, label = self.label, semantics = self.semantics)
 	return newExp
 
 testExp = Explanation(
         name='LF_spouse_to_left',
-        condition="there is a spouse word within two words to the left of arg 1 or arg 2",
+        condition="the word 'wife' is within two words to the left of arg 1 or arg 2",
         candidate='03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6822:6837~~03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6855:6858',
         label=True,
         semantics=None)
 
 testing = KeywordBaselines(testExp)
 print(testing.explanation)
-print(testing.modify("wife"))
+keyword=testing.findKeywords()
+print(keyword[0])
+print(testing.modify(keyword[0]))
