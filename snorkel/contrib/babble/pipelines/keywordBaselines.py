aee1174954911c3b3225d6b4e800b13e83c21d10
emilydiana <emily.r.diana@gmail.com>
2017-11-07 11:34:57 -0800

    Removing some etxra tests

--- snorkel/contrib/babble/pipelines/keywordBaselines.py
+++ snorkel/contrib/babble/pipelines/keywordBaselines.py
@@ -1,6 +1,6 @@
 from snorkel.contrib.babble import Explanation, link_explanation_candidates
 import re
-###from snorkel.contrib.babble.semparser import Explanation
+
 ### This file will contain a class definition to remove extra conditions
 ### from explanantions containing a string literal. These modified explanations
 ### will ultimately be used to construct baselines investigating whether
@@ -16,7 +16,7 @@ class KeywordBaselines:
 	self.candidate=explanation.candidate
 	self.label=explanation.label
 	self.semantics=explanation.semantics
-
+ 
     def findKeywords(self):
 	keyword=re.findall(r'\'(.+?)\'',self.condition)
 	return keyword
@@ -25,15 +25,15 @@ class KeywordBaselines:
 	newExp = Explanation(name = self.name, condition = keyword, candidate = self.candidate, label = self.label, semantics = self.semantics)
 	return newExp
 
-testExp = Explanation(
-        name='LF_spouse_to_left',
-        condition="the word 'wife' is within two words to the left of arg 1 or arg 2",
-        candidate='03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6822:6837~~03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6855:6858',
-        label=True,
-        semantics=None)
+#testExp = Explanation(
+#        name='LF_spouse_to_left',
+#        condition="the word 'wife' is within two words to the left of arg 1 or arg 2",
+#        candidate='03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6822:6837~~03a1e1a0-93c3-41a8-a905-a535ce8f2b09::span:6855:6858',
+#        label=True,
+#        semantics=None)
 
-testing = KeywordBaselines(testExp)
-print(testing.explanation)
-keyword=testing.findKeywords()
-print(keyword[0])
-print(testing.modify(keyword[0]))
+#testing = KeywordBaselines(testExp)
+#print(testing.explanation)
+#keyword=testing.findKeywords()
+#print(keyword[0])
+#print(testing.modify(keyword[0]))
