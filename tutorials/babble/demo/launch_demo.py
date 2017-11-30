766c25cca6a21f70ef260205ffd3c4ed19a07e71
Braden Hancock <hancock.braden@gmail.com>
2017-11-30 15:56:24 -0800

    Merge steph_babble_demo into babble branch.

---
+++ tutorials/babble/demo/launch_demo.py
@@ -0,0 +1,143 @@
+from flask import Blueprint, Flask, flash, redirect, render_template, request, session, abort, current_app
+import os
+import sys
+sys.path.append('/Users/Stephaniewang/Documents/snorkel')
+from snorkel import SnorkelSession
+from snorkel.contrib.babble.pipelines import merge_configs
+from snorkel.models import candidate_subclass
+from snorkel.contrib.babble import ExplanationIO
+from tutorials.babble.spouse import SpousePipeline
+from snorkel.viewer import SentenceNgramViewer
+from snorkel.contrib.babble import Explanation
+from snorkel.contrib.babble import Babbler
+
+from flask_bootstrap import Bootstrap
+from snorkel.contrib.babble import BabbleStream
+from snorkel.lf_helpers import *
+from tutorials.babble.spouse.spouse_examples import get_explanations, get_user_lists
+from main_page import main_page
+# dependencies:
+# flask
+# Flask-WTF
+# wtforms
+# flask_bootstrap
+ 
+def create_app(bs, c, session, pipe):
+	app = Flask(__name__)
+	app.secret_key = 'myverylongsecretkey'
+	bootstrap = Bootstrap(app)
+	app.config['babble_stream_object'] = bs
+	app.config['candidate'] = c
+	app.config['session'] = session
+	app.config['pipe'] = pipe
+	app.config['metrics'] = {'lf_stats': '', 'coverage': 0, 'num_labels': 0, 'num_examples': 0, 'num_labels_equiv': 0, 'f1': 0}
+	app.register_blueprint(main_page)
+	return app
+
+if __name__ == "__main__":
+	config = {
+	'b': 0.5,
+	'babbler_candidate_split': 0,
+	'babbler_label_split': 0,
+	'beam_width': 10,
+	'candidate_entities': ['person1', 'person2'],
+	'candidate_name': 'Spouse',
+	'db_name': 'babble_spouse_demo',
+	'debug': False,
+	'deps_thresh': 0.01,
+	'disc_eval_batch_size': 256,
+	'disc_init_params': {'n_threads': 16, 'seed': 123},
+	'disc_model_class': 'lstm',
+	'disc_model_search_space': 10,
+	'disc_params_default': {'batch_size': 128,
+	'dim': 50,
+	'dropout': 0.5,
+	'lr': 0.01,
+	'max_sentence_length': 100,
+	'n_epochs': 20,
+	'print_freq': 1,
+	'rebalance': 0.25},
+	'disc_params_range': {'dim': [50, 100],
+	'dropout': [0.1, 0.25, 0.5],
+	'lr': [0.01, 0.001, 0.0001],
+	'rebalance': [0.1, 0.25]},
+	'display_accuracies': True,
+	'display_correlation': False,
+	'display_learned_accuracies': False,
+	'display_marginals': True,
+	'do_filter_consistency': True,
+	'do_filter_duplicate_semantics': True,
+	'do_filter_duplicate_signatures': True,
+	'do_filter_uniform_signatures': True,
+	'domain': 'spouse',
+	'end_at': 10,
+	'gen_f_beta': 1,
+	'gen_init_params': {'class_prior': False,
+	'lf_class_propensity': False,
+	'lf_prior': False,
+	'lf_propensity': True,
+	'seed': 123},
+	'gen_model_search_space': 10,
+	'gen_params_default': {'decay': 0.99,
+	'epochs': 50,
+	'init_class_prior': -1.15,
+	'reg_param': 0.5,
+	'step_size': 0.0001},
+	'gen_params_range': {'LF_acc_prior_weight_default': [0.5, 1.0, 1.5],
+	'decay': [0.9, 0.95, 0.99],
+	'reg_param': [0.0, 0.01, 0.1, 0.25, 0.5],
+	'step_size': [0.01, 0.001, 0.0001, 1e-05]},
+	'learn_deps': False,
+	'log_dir': 'logs',
+	'max_docs': None,
+	'max_train': None,
+	'no_plots': False,
+	'parallelism': 1,
+	'postgres': False,
+	'reports_dir': 'reports',
+	'seed': 0,
+	'splits': [0, 1, 2],
+	'start_at': 0,
+	'supervision': 'generative',
+	'top_k': -1,
+	'verbose': True
+	}
+	# Get DB connection string and add to globals
+	# default_db_name = 'babble_' + config['domain'] + ('_debug' if config.get('debug', False) else '')
+	# DB_NAME = config.get('db_name', default_db_name)
+	# if 'postgres' in config and config['postgres']:
+	#     DB_TYPE = 'postgres'
+	# else:
+	#     DB_TYPE = 'sqlite'
+	#     DB_NAME += '.db'
+	# DB_ADDR = "localhost:{0}".format(config['db_port']) if 'db_port' in config else ""
+	# os.environ['SNORKELDB'] = '{0}://{1}/{2}'.format(DB_TYPE, DB_ADDR, DB_NAME)
+	# print("$SNORKELDB = {0}".format(os.environ['SNORKELDB']))
+
+	session = SnorkelSession()
+
+	# config = merge_configs(config)
+
+	# if config['debug']:
+	#     print("NOTE: --debug=True: modifying parameters...")
+	#     config['max_docs'] = 100
+	#     config['gen_model_search_space'] = 2
+	#     config['disc_model_search_space'] = 2
+	#     config['gen_params_default']['epochs'] = 25
+	#     config['disc_params_default']['n_epochs'] = 5
+
+	Spouse = candidate_subclass('Spouse', ['person1', 'person2'])
+	candidate_class = Spouse
+	pipe = SpousePipeline(session, Spouse, config)
+	# pipe.parse()
+	# pipe.extract()
+	# pipe.load_gold()
+
+	bs = BabbleStream(session, candidate_class=Spouse, balanced=True, seed=123, soft_start=True)
+	candidates = session.query(Spouse).filter(Spouse.split == 0).all()
+	# spouse_explanations = get_explanations()
+	# spouse_user_lists = get_user_lists()
+	# bs.preload(explanations=spouse_explanations, user_lists=spouse_user_lists)
+	c = bs.next()
+	app = create_app(bs, c, session, pipe)
+	app.run()
---
+++ tutorials/babble/demo/main_page.py
@@ -0,0 +1,235 @@
+from flask import current_app, Blueprint, Flask, flash, redirect, render_template, request, session
+import os
+import sys
+sys.path.append('/Users/Stephaniewang/Documents/snorkel')
+from snorkel import SnorkelSession
+from snorkel.contrib.babble.pipelines import merge_configs
+from snorkel.models import candidate_subclass
+from snorkel.contrib.babble import ExplanationIO
+from tutorials.babble.spouse import SpousePipeline
+from snorkel.viewer import SentenceNgramViewer
+from snorkel.contrib.babble import Explanation
+from snorkel.contrib.babble import Babbler
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, TextAreaField
+from wtforms.fields import RadioField, SubmitField, BooleanField
+from flask_bootstrap import Bootstrap
+from snorkel.contrib.babble import BabbleStream
+from snorkel.lf_helpers import *
+from tutorials.babble.spouse.spouse_examples import get_explanations, get_user_lists
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
+class ExplanationForm(FlaskForm):
+	explanation = TextAreaField('Explanation', [validators.DataRequired()])
+
+class LabelingFunctionForm(FlaskForm):
+	pass
+
+# https://stackoverflow.com/questions/46653424/flask-wtforms-fieldlist-with-booleanfield
+def parse_list_form_builder(parses, translator):
+	class ParseListForm(FlaskForm):
+		pass
+	for (idx, parse) in enumerate(parses):
+		label = "Parse {}:\n{}\n".format(idx, translator(parse.semantics))
+		setattr(ParseListForm, 'parse', BooleanField(label=label))
+	return ParseListForm()
+
+def candidate_html(c):
+	chunks = get_text_splits(c)
+	div_tmpl = u'''<div style="border: 1px #858585; box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    background-color:#FDFDFD; padding:5pt 10pt 5pt 10pt; width: 80%; margin: auto; margin-top: 2%">	
+    {}</div>'''
+	arg_tmpl = u'<b style="background-color:#ffd77c;padding:1pt 5pt 1pt 5pt;">{}</b>'
+	sent_tmpl = u'<p style="font-size:12pt;">{}</p>'
+	text = u""
+	for s in chunks[0:]:
+	    if s in [u"{{A}}", u"{{B}}"]:
+	        span = c[0].get_span() if s == u"{{A}}" else c[1].get_span()
+	        text += arg_tmpl.format(span)
+	    else:
+	        text += s.replace(u"\n", u"<BR/>")
+	html = div_tmpl.format(sent_tmpl.format(text.strip()))
+	return html
+
+def sentence_html(c):
+	chunks = get_text_splits(c)
+	arg_tmpl = u'<b style="padding:1pt 5pt 1pt 5pt;">{}</b>'
+	sent_tmpl = u'<p style="font-size:10pt;">{}</p>'
+	text = u""
+	for s in chunks[0:]:
+	    if s in [u"{{A}}", u"{{B}}"]:
+	        span = c[0].get_span() if s == u"{{A}}" else c[1].get_span()
+	        text += arg_tmpl.format(span)
+	    else:
+	        text += s.replace(u"\n", u"<BR/>")
+	html = sent_tmpl.format(text.strip())
+	return html
+
+def apply_filtered_analysis(filtered_parses, translator):
+	row_tmpl = u'<dl class="row">{}</dl>'
+	dt_header_tmpl = u'<dt class="col-sm-12">{}</dt>'
+	dt_tmpl = u'<dt class = "col-sm-3">{}</dt>'
+	dd_tmpl = u'<dd class= "col-sm-9">{}</dd>'
+	text = u""
+
+	if not any(filtered_parses.values()):
+		return None
+	num_filtered = 0
+	filter_names = [
+	'UnparseableExplanations',
+	'DuplicateSemanticsFilter',
+	'ConsistencyFilter',
+	'UniformSignatureFilter',
+	'DuplicateSignatureFilter']
+	for filter_name in filter_names:
+		parses = filtered_parses.get(filter_name, [])
+		for filtered_parse in parses:
+			num_filtered += 1
+
+			if filtered_parse.reason == 'Unparseable':
+			    parse_str = filtered_parse.parse.condition
+			else:
+			    parse_str = translator(filtered_parse.parse.semantics)
+
+			if filter_name == 'UnparseableExplanations':
+			    filter_str = "Unparseable Explanation"
+			    reason_str = "This explanation couldn't be parsed."
+
+			elif filter_name == 'DuplicateSemanticsFilter':
+			    filter_str = "Duplicate Semantics"
+			    reason_str = 'This parse is identical to one produced by the following explanation:\n\t"{}"'.format(filtered_parse.reason.explanation.condition)
+			    
+			elif filter_name == 'ConsistencyFilter':
+			    candidate = filtered_parse.reason
+			    filter_str = "Inconsistency with Example"
+			    reason_str = "This parse did not agree with the candidate ({}, {})".format(candidate[0].get_span(), candidate[1].get_span())
+			    
+			elif filter_name == 'UniformSignatureFilter':
+				filter_str = "Uniform Signature"
+				bs = current_app.config['babble_stream_object']
+				reason_str = "This parse labeled {} of the {} development examples".format(
+			        filtered_parse.reason, bs.num_dev_total)
+			    
+			elif filter_name == 'DuplicateSignatureFilter':
+			    filter_str = "Duplicate Signature"
+			    reason_str = "This parse labeled identically to the following existing parse:\n\t{}".format(
+			        translator(filtered_parse.reason.explanation))
+
+			text += '<br/>'
+			text += dt_header_tmpl.format("#{}: {}".format(num_filtered, filter_str))
+			if filtered_parse.reason == 'Unparseable':
+			    text += dt_tmpl.format("Explanation:")
+			    text += dd_tmpl.format(parse_str)
+			else:
+			    text += dt_tmpl.format("Parse:") 
+			    text += dd_tmpl.format(parse_str)
+			text += dt_tmpl.format("Reason:")
+			text +=  dd_tmpl.format(reason_str)
+			text += '<br/>'
+	html = row_tmpl.format(text.strip())
+	return html
+
+def get_metrics(bs):
+	# get the metrics to display
+	metrics = current_app.config['metrics']
+	num_examples = len(bs.get_explanations())
+	if num_examples < 1:
+		return metrics
+	metrics['num_examples'] = num_examples
+	metrics['lf_stats'] = bs.get_lf_stats().to_html(columns=['Coverage', 'Overlaps', 'TP', 'FP', 'FN', 'TN', 'Empirical Acc.'])
+	metrics['coverage'] = bs.get_global_coverage().numer/float(bs.get_global_coverage().denom) * 100
+	metrics['num_labels'] = len(bs.get_lfs())
+	return metrics
+
+def finish_pipeline(bs):
+		pipe = current_app.config['pipe']
+		pipe.set_babbler_matrices(bs)
+		pipe.supervise()
+		pipe.classify()
+
+main_page = Blueprint('main_page', __name__, template_folder='templates')
+
+@main_page.route("/", methods=['GET', 'POST'])
+def index():
+	bs = current_app.config['babble_stream_object']
+	form1 = ExplanationForm(request.form)
+	form2 = LabelingFunctionForm(request.form)
+
+	metrics = get_metrics(bs)
+
+	# CASE 1: SKIP THE CURRENT SAMPLE
+	if request.method == 'POST' and "skip" in request.form:
+		current_app.config['candidate'] = bs.next() # GET THE NEXT CANDIDATE
+		candidate = candidate_html(current_app.config['candidate'])
+		return redirect('/')
+
+	# CASE 2: SUBMIT AN EXPLANATION
+	print request.form
+	if request.method == 'POST' and form1.validate_on_submit() and ("pos" in request.form or "neg" in request.form):
+		candidate = candidate_html(current_app.config['candidate'])
+		if "pos" in request.form: label = True
+		elif "neg" in request.form: label = False
+		condition = form1.explanation.data
+		num_explanations = len(bs.get_explanations())
+		explanation = Explanation(
+			condition, 
+			label, 
+			candidate=current_app.config['candidate'],
+			name='Exp{}:{}...'.format(num_explanations,condition[:25])
+		)
+
+		###### for debugging purposes ######
+		print current_app.config['candidate']
+		print label
+		print condition
+		print explanation
+		###################################
+
+		parse_results = bs.apply(explanation)
+		print len(parse_results)
+		if len(parse_results[0]) > 0:
+			parse_list, filtered_parses, conf_matrix_list, stats_list = parse_results
+			parse_list = [bs.semparser.grammar.translate(parse.semantics) for parse in parse_list]
+			correct_incorrect_sentences = []
+			for idx in range(len(conf_matrix_list)):
+				tf_sentence_dict = {}
+				tf_sentence_dict["correct"] = [sentence_html(sentence) for sentence in conf_matrix_list[idx].correct]
+				tf_sentence_dict["incorrect"] = [sentence_html(sentence) for sentence in conf_matrix_list[idx].incorrect]
+				tf_sentence_dict["abstain"] = [sentence_html(sentence) for sentence in conf_matrix_list[idx].abstained]
+				correct_incorrect_sentences.append(tf_sentence_dict)
+
+			filtered_analysis = apply_filtered_analysis(filtered_parses, bs.semparser.grammar.translate)
+
+			displayed_stats = zip(parse_list, correct_incorrect_sentences, stats_list)
+			return render_template('index.html', candidate=candidate, form=form1, stats=displayed_stats, filtered_analysis=filtered_analysis, form2=form2, metrics=metrics)
+		else:
+			# generated no new parses
+			filtered_parses = parse_results[1]
+			filtered_analysis = apply_filtered_analysis(filtered_parses, bs.semparser.grammar.translate)
+			return render_template('index.html', candidate=candidate, form=form1, no_parse_error = "True", filtered_analysis=filtered_analysis, metrics=metrics)
+
+	# CASE 3: COMMIT THE LFS 
+	elif request.method == 'POST' and "commit" in request.form:
+		bs.commit()
+
+		# update the metrics
+		(f1, pr, re) = bs.get_majority_quality(split=1)
+		metrics = current_app.config['metrics']
+		metrics['f1'] = f1
+		metrics['num_labels_equiv'] = bs.get_labeled_equivalent(f1)
+
+		# get the next candidate
+		current_app.config['candidate'] = bs.next()
+		flash('You\'ve successfully committed your explanation', 'success')
+		return redirect('/')
+
+	# CASE 4: FINISH THE PIPELINE -- LABEL DATASET AND TRAIN MODEL
+	elif request.method == 'POST' and "finish" in request.form:
+		finish_pipeline(bs)
+		
+	candidate = candidate_html(current_app.config['candidate'])
+	return render_template('index.html', candidate=candidate, form=form1, metrics=metrics)
+
