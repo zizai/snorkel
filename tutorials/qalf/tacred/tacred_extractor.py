83e80f9e3b4ab8e1fcf5f46206124ff93b8e1167
Braden Hancock <hancock.braden@gmail.com>
2018-01-13 16:22:46 -0800

    WIP: Successfully extracting Tacred sentences.

--- tutorials/qalf/tacred/tacred_extractor.py
+++ tutorials/qalf/tacred/tacred_extractor.py
@@ -1,11 +1,14 @@
 import os
 
+import numpy as np
+
 from snorkel.models import Context, Document, Sentence, Candidate
 from snorkel.models import construct_stable_id
 from snorkel.udf import UDF, UDFRunner
 
 from tutorials.qalf.tacred import TacredReader
 
+PTB = {'-RRB-': ')', '-LRB-': '(', '-RCB-': '}', '-LCB-': '{', '-RSB-': ']', '-LSB-': '['}
 
 class TacredExtractor(UDFRunner):
 
@@ -34,15 +37,52 @@ class TacredExtractorUDF(UDF):
         Args:
             xs: Iterator of TacredExamples
         """
+        doc = self._create_doc(example)
+        sentence = self._create_sentence(example, doc)
+        # Create candidate
+        # Store gold
+        yield sentence
+
+    def _create_doc(self, example):
         # Create one Document per TacredExample
+
         stable_id = "{}::document:0:0".format(example.uid)
-        doc = Document(
-            name=example.uid, stable_id=stable_id,
-            meta={'doc_id': example.doc_id}
-        )
-        yield doc
-            # create Sentence
-            # create Candidate
-            # store Gold
-        # Don't forget to commit!
-        # print(i)
+        return Document(name=example.uid, 
+                        stable_id=stable_id,
+                        meta={'doc_id': example.doc_id})
+
+    def _create_sentence(self, example, doc):
+        # Create one Sentence per Document
+
+        (indices, tokens, subjs, subj_types, objs, obj_types, pos_tags, 
+            ner_tags, _, dep_parents) = zip(*(example.tokens))
+        
+        parts = {
+            'words': [PTB.get(t, t) for t in tokens],
+            'pos_tags': pos_tags,
+            'ner_tags': ner_tags,
+            'dep_parents': dep_parents,
+        }
+        word_lengths_with_space = map(lambda x: len(x) + 1, tokens)
+        char_offsets = list(np.cumsum([0] + word_lengths_with_space[:-1]))
+        parts['char_offsets'] = char_offsets
+        parts['abs_char_offsets'] = char_offsets
+
+        parts['entity_cids'] = ['O' for _ in parts['words']]
+        entity_types = []
+        for i, (subj, obj) in enumerate(zip(subjs, objs)):
+            if subj == 'SUBJECT':
+                entity_types.append('Subject')
+            elif obj == 'OBJECT':
+                entity_types.append('Object')
+            else:
+                entity_types.append('O')
+        parts['entity_types'] = entity_types
+
+        text = ' '.join(tokens)
+        parts['document'] = doc
+        parts['position'] = 0  # Each Sentence is the first in its Document
+        parts['text'] = text
+        parts['stable_id'] = construct_stable_id(doc, 'sentence', 0, len(text))
+        
+        return Sentence(**parts)
