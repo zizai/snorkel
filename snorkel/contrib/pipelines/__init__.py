14d04db535292969d17c95df44e97055cc871843
Braden Hancock <hancock.braden@gmail.com>
2018-01-19 13:02:01 -0800

    Fix import errors.

--- snorkel/contrib/pipelines/__init__.py
+++ snorkel/contrib/pipelines/__init__.py
@@ -1,4 +1,4 @@
+from .utils import STAGES, TRAIN, DEV, TEST, final_report, score_marginals, sparse_to_labelmatrix
 from .config import global_config
 from .config_utils import merge_configs, get_local_pipeline
-from .utils import STAGES, TRAIN, DEV, TEST, final_report, score_marginals, sparse_to_labelmatrix
 from .snorkel_pipeline import SnorkelPipeline
