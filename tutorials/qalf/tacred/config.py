8362bac11302d6e648c017a074b44a9bdb2f33d7
Braden Hancock <hancock.braden@gmail.com>
2018-01-26 09:43:14 -0800

    Save cleaned qalf config files.

--- tutorials/qalf/tacred/config.py
+++ tutorials/qalf/tacred/config.py
@@ -5,25 +5,15 @@ config = {
     'splits': [0, 1],
 
     # Classifier
-    'disc_model_class': 'lstm',
-    'disc_model_search_space': 10,
-	'disc_init_params': {
-        'n_threads': 16,
-        'seed'     : None,
-    },
+    'disc_model_class': 'logreg',
     'disc_params_range': {
-        'lr'        : [1e-2, 1e-3, 1e-4],
-        'n_epochs'  : [10, 20, 50],
-        'batch_size': [32, 64, 128, 256],
+        'n_epochs'  : [25, 50, 100],
     },
     'disc_params_default': {
         'lr':         0.01,
-        'dim':        50,
-        'n_epochs':   20,
-        'dropout':    0.5,
+        'n_epochs':   100,
         'rebalance':  False,
-        'batch_size': 128,
-        'max_sentence_length': 200,
+        'batch_size': 64,
         'print_freq': 1,
     },
     'disc_eval_batch_size': 256,    
