416fc31b2829f9b827df5be22624b26067a5a5af
Braden Hancock <hancock.braden@gmail.com>
2018-01-31 12:28:36 -0800

    Add missing comma.

--- snorkel/contrib/pipelines/config.py
+++ snorkel/contrib/pipelines/config.py
@@ -95,7 +95,7 @@ global_config = {
     'disc_params_range': {
         'lr'        : [1e-1, 1e-2, 1e-3, 1e-4],
         'dim'       : [32, 64, 128],
-        'batch_size': [32, 64, 128]
+        'batch_size': [32, 64, 128],
         'l1_penalty': [0, 1e-5, 1e-4, 1e-3, 1e-2], 
         'l2_penalty': [0, 1e-5, 1e-4, 1e-3, 1e-2], 
     },
