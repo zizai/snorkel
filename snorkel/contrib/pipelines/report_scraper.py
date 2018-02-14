60ea1a31a12835f82600aae933d5b8db97dea996
Braden Hancock <hancock.braden@gmail.com>
2018-02-14 15:21:58 -0800

    Add option for report_scraper to print pr and re as well as f1.

--- snorkel/contrib/pipelines/report_scraper.py
+++ snorkel/contrib/pipelines/report_scraper.py
@@ -7,6 +7,7 @@ parameters = ["max_train"]
 
 date_str = sys.argv[1] # e.g., '02_13_18'                                                        
 exp_name = sys.argv[2] # e.g., 'cdr_finaltradit0'
+include_all = True if sys.argv[3].lower().startswith('t)') else False # e.g., True
 
 report_subdir = os.path.join(os.environ['SNORKELHOME'], 'reports', date_str)
 reports = []
@@ -20,14 +21,19 @@ results = defaultdict(list)
 for report in reports:
     data = json.load(open(report))
     f1 = data["scores"]["F1 Score"]["Disc"]
+    r = data["scores"]["Recall"]["Disc"]
+    p = data["scores"]["Precision"]["Disc"]
     settings = []
     for parameter in parameters:
         settings.append(data["config"][parameter])
-    results[tuple(settings)].append(f1)
+    if include_all:
+        results[tuple(settings)].append((p, r, f1))
+    else:
+        results[tuple(settings)].append(f1)
 
-for settings, scores in sorted(results.items()):
+for settings, scores_list in sorted(results.items()):
     for param, val in zip(parameters, settings):
         print("{} = {}".format(param, val))
-    for score in scores:
-        print(score)
+    for scores in scores_list:
+        print('\t'.join(map(str, scores)))
     print("")
