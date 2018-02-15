52475374f6cef0144daf49c58383dda604270ab1
Braden Hancock <hancock.braden@gmail.com>
2018-02-15 13:32:23 -0800

    Soup up report_scraper

--- snorkel/contrib/pipelines/report_scraper.py
+++ snorkel/contrib/pipelines/report_scraper.py
@@ -1,39 +1,48 @@
+import argparse
 from collections import defaultdict
 import json
 import os
 import sys
 
-parameters = ["max_train"]
+def main(args):
+    report_subdir = os.path.join(os.environ['SNORKELHOME'], 'reports', args['date'])
+    reports = []
+    for root, dirs, files in os.walk(report_subdir):
+        if args['exp'] in root:
+            for filename in files:
+                if filename.endswith('json'):
+                    reports.append(root + '/' + filename)
 
-date_str = sys.argv[1] # e.g., '02_13_18'                                                        
-exp_name = sys.argv[2] # e.g., 'cdr_finaltradit0'
-include_all = True if sys.argv[3].lower().startswith('t') else False # e.g., True
+    results = defaultdict(list)
+    for report in reports:
+        data = json.load(open(report))
+        f1 = data["scores"]["F1 Score"]["Disc"]
+        r = data["scores"]["Recall"]["Disc"]
+        p = data["scores"]["Precision"]["Disc"]
+        settings = []
+        for parameter in args['params']:
+            settings.append(data["config"][parameter])
+        if args['f1']:
+            results[tuple(settings)].append((f1,))
+        else:
+            results[tuple(settings)].append((p, r, f1))
 
-report_subdir = os.path.join(os.environ['SNORKELHOME'], 'reports', date_str)
-reports = []
-for root, dirs, files in os.walk(report_subdir):
-    if exp_name in root:
-        for filename in files:
-            if filename.endswith('json'):
-                reports.append(root + '/' + filename)
+    for settings, scores_list in sorted(results.items()):
+        for param, val in zip(args['params'], settings):
+            print("{} = {}".format(param, val))
+        for scores in scores_list:
+            print('\t'.join(map(str, scores)))
+        print("")
 
-results = defaultdict(list)
-for report in reports:
-    data = json.load(open(report))
-    f1 = data["scores"]["F1 Score"]["Disc"]
-    r = data["scores"]["Recall"]["Disc"]
-    p = data["scores"]["Precision"]["Disc"]
-    settings = []
-    for parameter in parameters:
-        settings.append(data["config"][parameter])
-    if include_all:
-        results[tuple(settings)].append((p, r, f1))
-    else:
-        results[tuple(settings)].append((f1,))
 
-for settings, scores_list in sorted(results.items()):
-    for param, val in zip(parameters, settings):
-        print("{} = {}".format(param, val))
-    for scores in scores_list:
-        print('\t'.join(map(str, scores)))
-    print("")
+if __name__ == '__main__':
+    # Parse command-line args
+    argparser = argparse.ArgumentParser(description="Record scraper.")
+    argparser.add_argument('--exp', type=str)
+    argparser.add_argument('--date', type=str, default='02_15_18')
+    argparser.add_argument('--f1', action='store_true')
+    argparser.add_argument('--params', type=str, nargs='*', default='max_train')
+    args = vars(argparser.parse_args())
+    main(args)
+
+
